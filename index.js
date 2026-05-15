7210f9ecd8a72315113ff5dc769e77812a5812a9
edupoly <info.edupoly@gmail.com>
2026-05-15 10:21:31 +0530

    socketcommunication

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ var { Server } = require("socket.io");
 
 var app = express();
 var server = http.createServer(app);
-var io = new Server(server);
 
-app.listen(3500, () => {
+var io = new Server(server);
+var liveUserCount = 0;
+io.on("connection", (socket) => {
+  socket.username = socket.handshake.query.username;
+  liveUserCount++;
+  io.emit("liveCountEvent", { uc: liveUserCount });
+
+  socket.on("disconnect", () => {
+    liveUserCount--;
+    io.emit("liveCountEvent", { uc: liveUserCount, greeting: "Bagunnara sir" });
+  });
+  socket.on("userMessage", (ms) => {
+    console.log(socket.username + " :: " + ms.msg);
+
+    io.emit("groupMessage", { ms, username: socket.username });
+  });
+});
+
+app.use(express.static(__dirname + "/general"));
+
+server.listen(3500, () => {
   console.log("Server uriking on 3500");
 });
